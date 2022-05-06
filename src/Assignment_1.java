5b31df1d6528cbfc871e8af1cfe95b4610b832ed
Mcabillah <christophermoris255@gmail.com>
2022-05-06 20:28:43 +0300

    Lab 1 Assignment_1

---
+++ src/Assignment_1.java
@@ -0,0 +1,11 @@
+public class Assignment_1 {
+    public static void main(String[] args){
+        double a = -9.81;
+        double Vi = 0;
+        double Xi = 0;
+        double Xt;
+        int t = 10;
+        Xt = 0.5*a*t*t + Vi*t + Xi;
+        System.out.println("The value of Xt is: "+Xt);
+    }
+}
